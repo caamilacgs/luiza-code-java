68ede99eaf2a679fe9fa366ba57f05343c0a2626
Camila <camilacgs17@gmail.com>
2021-05-01 19:44:17 -0300

    Use a Cabeça

---
+++ src/com/exerciciosLivres/UseCabeca/Captulo1.java
@@ -0,0 +1,30 @@
+package com.luizacode.Exercicios.UseCabeca;
+
+public class Captulo1 {
+
+    public static void main(String[] args) {
+
+/*
+
+	int size = 27;`
+	String name = "Camila";
+	Dog myDog = new Dog(name, size);
+	int x = size - 5;
+	if (x < 15) myDog.bark(8);
+
+
+	int a = 4;
+	while (a > 3) {
+		myDog.play();
+	}
+
+	int[] numList = {2, 4, 6, 8};
+	System.out.print("Hello");
+	System.out.print("Dog:" + name);
+	String num = "8";
+	int z = integer.parseInt(num);
+*/
+
+    }
+
+}
---
+++ src/com/exerciciosLivres/UseCabeca/MyfirstApp.java
@@ -0,0 +1,10 @@
+package com.luizacode.Exercicios.UseCabeca;
+
+public class MyfirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("Naruto" + " x " + "Sasuke");
+        System.out.println("Blinblinblin");
+
+    }
+}
